d784c19a32cb37ea94816e614c86547f410dc96f
johnsonisaacpeter <ijohnson@student.neumont.edu>
2023-01-31 14:19:08 -0700

    set up roulette game

--- roulette-game.py
+++ roulette-game.py
@@ -1,1 +1,22 @@
-shit and cum
+import pygame
+
+class Bet:
+    def __init__(self, amount, number = None, color = None, even = None, odd = None, range = None):
+        self.amount = amount
+        self.number = number
+        self.color = color
+        self.even = even
+        self.odd = odd
+        self.range = range
+        
+    def payout(self):
+        pass        
+
+class Slot:
+    def __init__(self, number, color, cord):
+        self.number = number
+        self.color = color
+        self.cord = cord
+    
+def start_game():
+    pass
