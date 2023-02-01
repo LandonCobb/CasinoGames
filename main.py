5c4239cd6a17a0f511bbf61dbdb44cfc30d51508
Ben-Casillas <98473776+Ben-Casillas@users.noreply.github.com>
2023-02-01 13:56:14 -0700

    py files for other game logic

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import pygame
 import pygame_menu
+import roulette_game
 
 # while True:
 #    print('poop')
 
 pygame.init()
-surface = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+menu_screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+roulette_screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
 
 def roulette():
+    print(name)
+    main_menu.close()
+    main_menu.mainloop(roulette_screen)
     pass
 
 
@@ -24,13 +29,14 @@ def playerAccount():
     pass
 
 
-menu = pygame_menu.Menu('Welcome', 800, 600, theme=pygame_menu.themes.THEME_BLUE)
+main_menu = pygame_menu.Menu('Welcome', 1700, 1050, theme=pygame_menu.themes.THEME_BLUE)
+roulette_menu = pygame_menu.Menu('Welcome', 1700, 1050, theme=pygame_menu.themes.THEME_BLUE)
 
-menu.add.text_input('Name :', default="LeBron James")  # can store this value somewhere
-menu.add.button('Roulette Table', roulette)
-menu.add.button('Black Jack', leonardo)
-menu.add.button('Craps', craps)
-menu.add.button('See Balance', playerAccount)
-menu.add.button('Quit', pygame_menu.events.EXIT)
+name = main_menu.add.text_input('Name :', default="LeBron James").get_value()  # can store this value somewhere
+main_menu.add.button('Roulette Table', roulette_game.start_game)
+main_menu.add.button('Black Jack', leonardo)
+main_menu.add.button('Craps', craps)
+main_menu.add.button('See Balance', playerAccount)
+main_menu.add.button('Quit', pygame_menu.events.EXIT)
 
-menu.mainloop(surface)
+main_menu.mainloop(menu_screen)
