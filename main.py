c0679213059c5b9697b4e48f1a366c43a46ef908
KBierman <100715811+KBierman@users.noreply.github.com>
2023-02-04 15:37:04 -0700

    Slotss base

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-import pygame, pyautogui, pygame_menu, roulette_game
+import pygame, pyautogui, pygame_menu, roulette_game, slots
 
 pygame.init()
 screen_width,screen_height = pyautogui.size()
 base_screen = pygame.display.set_mode((screen_width, screen_height), pygame.FULLSCREEN)
 pygame.display.update()
-
-def roulette():
-    print(name)
-    main_menu.close()
-    main_menu.mainloop(base_screen)
-    pass
 
 def leonardo():
     pass
@@ -19,18 +13,24 @@ def craps():
 
 def playerAccount():
     pass
-
-mainTheme = pygame_menu.Theme(background_color=(181, 129, 214, 255),
-                title_background_color=(109, 70, 156),
-                title_font_shadow=True)
+myimage = pygame_menu.baseimage.BaseImage(
+    image_path="menu_assets\casino.png"
+)
+mainTheme = pygame_menu.Theme(
+                background_color = myimage,
+                title_font_shadow=True,
+                title_bar_style=pygame_menu.widgets.MENUBAR_STYLE_SIMPLE,
+                widget_font=pygame_menu.font.FONT_HELVETICA,
+                widget_font_color= (112,131,111),
+                widget_font_size=90)
 
 main_menu = pygame_menu.Menu('Welcome to a casino', screen_width, screen_height, theme=mainTheme)
 
 name = main_menu.add.text_input('Name :', default="LeBron James").get_value()  # can store this value somewhere
 main_menu.add.button('Roulette Table', roulette_game.start_game)
 main_menu.add.button('Black Jack', leonardo)
+main_menu.add.button('Slots', slots.start_game)
 main_menu.add.button('Craps', craps)
 main_menu.add.button('See Balance', playerAccount)
 main_menu.add.button('Quit', pygame_menu.events.EXIT)
-
 main_menu.mainloop(base_screen)
